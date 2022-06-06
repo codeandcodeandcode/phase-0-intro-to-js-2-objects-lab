7259fb883a14e91e47c92435c60ac06475ce72f7
codeandcodeandcode <codepkcode49@gmail.com>
2022-06-06 20:43:56 +0300

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "John",
+    streetAddress: "China",
+}
+
+function updateEmployeeWithKeyAndValue(eobj, key, value) {
+    const newObj = {...eobj};
+    newObj[key] = value;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(eobj, key, value) {
+eobj[key] = value;
+return eobj;
+}
+function deleteFromEmployeeByKey(object, key) {
+    // let keys = Object.keys(object);
+    const newObj = {...object};
+    delete newObj.key;
+    return newObj
+    
+    }
+    
+    function destructivelyDeleteFromEmployeeByKey(object, key) {
+    delete object.key;
+    return object;
+    }
